6f679e8669f1cd204a467e9e9389f270ceedf2c0
Christian Sieger <csieger@live.de>
2019-02-17 21:05:28 +0100

    add toxkey unit tests

---
+++ SharpTox.Tests/Core/Model/ToxKey.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using SharpTox.Core;
+
+namespace SharpTox.Tests
+{
+    [TestFixture]
+    public class ToxKey_Test
+    {
+        [Test]
+        public void Constructor_ByteArrayNotOfLengthPublicToxKeyLength_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new ToxKey(ToxKeyType.Public, new byte[ToxConstants.PublicKeySize + 1]));
+        }
+
+        [Test]
+        public void Constructor_ByteArrayNotOfLengthSecretToxKeyLength_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new ToxKey(ToxKeyType.Secret, new byte[ToxConstants.SecretKeySize + 1]));
+        }
+
+        [TestCase(ToxKeyType.Public, ToxConstants.PublicKeySize)]
+        [TestCase(ToxKeyType.Secret, ToxConstants.SecretKeySize)]
+        public void GetBytes_InputByteArray_AreNotSame(ToxKeyType keyType, int keySize)
+        {
+            var input = new byte[keySize];
+            var key = new ToxKey(keyType, input);
+            var result = key.GetBytes();
+
+            Assert.AreNotSame(input, result);
+        }
+
+        [TestCase(ToxKeyType.Public, ToxConstants.PublicKeySize)]
+        [TestCase(ToxKeyType.Secret, ToxConstants.SecretKeySize)]
+        public void Equal_SameByteArray_IsTrue(ToxKeyType keyType, int keySize)
+        {
+            var key = new byte[keySize];
+            var key1 = new ToxKey(keyType, key);
+            var key2 = new ToxKey(keyType, key);
+            var equal = key1 == key2;
+            Assert.IsTrue(equal);
+        }
+    }
+}
--- SharpTox/Core/Model/ToxTools.cs
+++ SharpTox/Core/Model/ToxTools.cs
@@ -8,9 +8,9 @@ namespace SharpTox.Core
     /// </summary>
     public static class ToxTools
     {
-        internal static string HexBinToString(byte[] b)
+        public static string HexBinToString(byte[] b)
         {
-            StringBuilder sb = new StringBuilder(2 * b.Length);
+            var sb = new StringBuilder(2 * b.Length);
 
             for (int i = 0; i < b.Length; i++)
                 sb.AppendFormat("{0:X2}", b[i]);
@@ -18,7 +18,7 @@ namespace SharpTox.Core
             return sb.ToString();
         }
 
-        internal static byte[] StringToHexBin(string s)
+        public static byte[] StringToHexBin(string s)
         {
             byte[] bin = new byte[s.Length / 2];
 
@@ -28,48 +28,7 @@ namespace SharpTox.Core
             return bin;
         }
 
-        internal static DateTime EpochToDateTime(ulong epoch)
-        {
-            return new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(Convert.ToDouble(epoch));
-        }
-
-        /// <summary>
-        /// Generates a cryptographic hash of the given data.
-        /// </summary>
-        /// <param name="data">The data to calculate the hash of.</param>
-        /// <returns>The cryptographic hash of the given data.</returns>
-        public static byte[] Hash(byte[] data)
-        {
-            if (data == null)
-                throw new ArgumentNullException("data");
-
-            byte[] hash = new byte[ToxConstants.HashLength];
-            ToxFunctions.Hash(hash, data, (uint)data.Length);
-            return hash;
-        }
-
-        internal static string RemoveNull(string s)
-        {
-            if (s.Length != 0)
-            {
-                int index = s.IndexOf(Char.MinValue);
-                if (!(index >= 0))
-                    return s;
-                else
-                    return s.Substring(0, index);
-            }
-
-            return s;
-        }
-
-        internal static uint Map(int i)
-        {
-            return unchecked((uint)i);
-        }
-
-        internal static int Map(uint i)
-        {
-            return unchecked((int)i);
-        }
+        public static DateTime EpochToDateTime(ulong epoch)
+            => new DateTime(1970, 1, 1).AddSeconds(Convert.ToDouble(epoch));
     }
 }
