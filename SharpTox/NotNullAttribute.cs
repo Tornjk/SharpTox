b3d3e8e0ef9a2af531f6a3853d55359570438917
Christian Sieger <csieger@live.de>
2019-02-19 20:09:26 +0100

    add fody and nullguard references

---
+++ SharpTox/NotNullAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpTox
+{
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue | AttributeTargets.Property)]
+    class NotNullAttribute : Attribute
+    {
+    }
+}
